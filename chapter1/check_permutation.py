8a7130bb0ac02eeded0795ff801305c5345e3d60
wenbin wu <dev@wuwenb.in>
2020-07-12 12:44:13 -0400

    Update check_permutation.py

--- chapter1/check_permutation.py
+++ chapter1/check_permutation.py
@@ -1,7 +1,7 @@
 def is_permu(mystr1: str, mystr2: str) -> bool:
     if not len(mystr1) == len(mystr2):
         return False
-    return True if set(mystr1) == set(mystr2) else False
+    return set(mystr1) == set(mystr2)
 
 
 if __name__ == '__main__':
